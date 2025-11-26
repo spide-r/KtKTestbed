49ef09f626c0af91184f5676d5631c08e72e9555
Spider <29214314+spide-r@users.noreply.github.com>
2025-11-26 09:19:01 -0600

    test bed start - ktk stuff works to put ui elements

---
+++ KtKTestBed/KtKTestBedPlugin.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Numerics;
+using Dalamud.Game.Command;
+using Dalamud.Interface.Windowing;
+using Dalamud.Plugin;
+using KamiToolKit;
+
+namespace KtKTestBed;
+
+public class KtKTestBedPlugin: IDalamudPlugin
+{
+
+    public WindowSystem WindowSystem = new("KtKTestBedPlugin");
+    
+
+    public IDalamudPluginInterface PluginInterface { get; set; }
+
+    public KtKTestBedPlugin(IDalamudPluginInterface pluginInterface)
+    {
+        PluginInterface = pluginInterface;
+            Service.Initialize(pluginInterface);
+            KamiToolKitLibrary.Initialize(pluginInterface);
+
+            pluginInterface.UiBuilder.Draw += DrawUi;
+            LoadCommands();
+
+    }
+    
+    private void LoadCommands()
+    {
+        Service.CommandManager.AddHandler("/test", new CommandInfo(OnCommand)
+        {
+            HelpMessage = "Test Stuff",
+        });
+    }
+    
+    private AddonWhmGauge? whmGauge;
+    private void OnCommand(string command, string args)
+    {
+        Service.ChatGui.Print("AAAAAAAAAAA TEST START!!!!!!");
+
+        whmGauge = new AddonWhmGauge {
+            
+            //NativeController = System.NativeController,
+            InternalName = "WhiteMageGauge",
+            Title = "White Mage Gauge",
+            Size = new Vector2(200.0f, 100.0f),
+        };
+        
+        whmGauge.Open();
+        Service.ChatGui.Print("AAAAAAAAAAA TEST FAILED!!!!!!");
+    }
+
+    
+
+
+    private void DrawUi()
+    {
+        WindowSystem.Draw();
+    }
+
+
+
+    public void Dispose()
+    {
+        KamiToolKitLibrary.Dispose();
+        PluginInterface.UiBuilder.Draw -= DrawUi;
+    }
+}
---
+++ KtKTestBed/Service.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Windows.Input;
+using Dalamud.IoC;
+using Dalamud.Plugin;
+using Dalamud.Plugin.Services;
+
+namespace KtKTestBed;
+
+#pragma warning disable 8618
+// ReSharper disable UnusedAutoPropertyAccessor.Local
+
+internal class Service {
+
+    [PluginService]
+    internal static IDataManager DataManager { get; private set; }
+    
+    [PluginService]
+    internal static ICommandManager CommandManager { get; private set; }
+    
+    [PluginService]
+    internal static IAddonLifecycle AddonLifecycle { get; private set; }
+
+    [PluginService]
+    internal static IChatGui ChatGui { get; private set; }
+    
+    [PluginService]
+    internal static IPluginLog PluginLog { get; private set; }
+
+    [PluginService]
+    internal static IObjectTable ObjectTable { get; private set; }
+
+    [PluginService]
+    internal static IPartyList PartyList { get; private set; }
+
+    [PluginService]
+    internal static IClientState ClientState { get; private set; }
+    
+    [PluginService]
+    internal static IDalamudPluginInterface DalamudPluginInterface { get; private set; }
+    
+    [PluginService]
+    internal static ICondition Condition { get; private set; }
+    
+    [PluginService]
+    internal static IFramework Framework { get; private set; }
+    
+    [PluginService]
+    internal static IGameInteropProvider GameInteropProvider { get; private set; }
+    
+    [PluginService]
+    internal static IDutyState DutyState { get; private set; }
+
+
+    internal static void Initialize(IDalamudPluginInterface pluginInterface) {
+
+        pluginInterface.Create<Service>();
+        
+        PluginLog.Verbose("Verbose");
+        PluginLog.Debug("Debug");
+        PluginLog.Info("Info");
+    }
+
+
+}
+#pragma warning restore 8618
