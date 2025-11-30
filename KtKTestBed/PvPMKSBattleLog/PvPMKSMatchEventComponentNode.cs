effa322d1785343542ab2def1a15c697d3c1bc39
Spider <29214314+spide-r@users.noreply.github.com>
2025-11-29 18:00:15 -0600

    finalized Match Event Component node

--- KtKTestBed/PvPMKSBattleLog/PvPMKSMatchEventComponentNode.cs
+++ KtKTestBed/PvPMKSBattleLog/PvPMKSMatchEventComponentNode.cs
@@ -13,10 +13,13 @@ public class PvPMKSMatchEventComponentNode: ResNode //this is a Base Component N
     private ResNode _unkResNode2 = null!;
     private ResNode _unkResNode3 = null!;
     
+    
+    
     private NineGridNode _unkNineGridNode27 = null!;
     private TextNode _unkTextNode26 = null!;
+    private ImageNode _unkImageNode25 = null!;
     private ResNode _unkResNode24 = null!;
-    private ResNode _unkBaseComponentNode23 = null!; //basecomponent-but-actually-resnode todo: MAKE INTO NEW CLASS
+    private ResNode _unkBaseComponentNode23 = null!; //basecomponent
     
     private ImageNode _unkImageNode22 = null!;
     private ImageNode _unkImageNode21 = null!;
@@ -29,7 +32,7 @@ public class PvPMKSMatchEventComponentNode: ResNode //this is a Base Component N
     
     private NineGridNode _unkNineGridNode14 = null!;
     
-    private ResNode _unkBaseComponentNode13 = null!; //basecomponent-but-actually-resnode todo: MAKE INTO NEW CLASS
+    private ResNode _unkBaseComponentNode13 = null!; //basecomponent
     
     private ImageNode _unkImageNode12 = null!;
     private ImageNode _unkImageNode11 = null!;
@@ -48,9 +51,47 @@ public class PvPMKSMatchEventComponentNode: ResNode //this is a Base Component N
         Size = new Vector2(384,40);
         NodeFlags = NodeFlags.AnchorTop | NodeFlags.AnchorLeft | NodeFlags.Visible | NodeFlags.Enabled |
                     NodeFlags.EmitsEvents;
-        // construct objects
-        // load timelines
-        // attach nodes
+        ConstructResNodes();
+        ConstructImageNodes();
+        ConstructNinegridNodes();
+        ConstructTextNodes();
+        LoadTimelineForImageNodes();
+        LoadTimelineForNineGridNodes();
+        LoadTimelineForResnodes();
+        LoadTimelineForTextnodes();
+        LoadTimelineForRoot();
+        AttachNodes();
+    }
+
+    private void AttachNodes()
+    {
+        _unkResNode2.AttachNode(this);
+        _unkResNode3.AttachNode(_unkResNode2);
+        
+        _unkTextNode4.AttachNode(_unkResNode3);
+        _unkTextNode5.AttachNode(_unkResNode3);
+        _unkTextNode6.AttachNode(_unkResNode3);
+        _unkResNode7.AttachNode(_unkResNode3);
+        _unkTextNode8.AttachNode(_unkResNode7);
+        _unkImageNode9.AttachNode(_unkResNode3);
+        _unkImageNode10.AttachNode(_unkResNode3);
+        _unkImageNode11.AttachNode(_unkResNode3);
+        _unkImageNode12.AttachNode(_unkResNode3);
+        _unkBaseComponentNode13.AttachNode(_unkResNode3);
+        _unkNineGridNode14.AttachNode(_unkResNode3);
+        _unkImageNode15.AttachNode(_unkResNode3);
+        _unkImageNode16.AttachNode(_unkResNode3);
+        _unkImageNode17.AttachNode(_unkResNode3);
+        _unkImageNode18.AttachNode(_unkResNode3);
+        _unkImageNode19.AttachNode(_unkResNode3);
+        _unkImageNode20.AttachNode(_unkResNode3);
+        _unkImageNode21.AttachNode(_unkResNode3);
+        _unkImageNode22.AttachNode(_unkResNode3);
+        _unkBaseComponentNode23.AttachNode(_unkResNode3);
+        _unkResNode24.AttachNode(_unkResNode3);
+        _unkImageNode25.AttachNode(_unkResNode24);
+        _unkTextNode26.AttachNode(_unkResNode3);
+        _unkNineGridNode27.AttachNode(_unkResNode3);
     }
 
     private void ConstructNinegridNodes()
@@ -79,6 +120,14 @@ public class PvPMKSMatchEventComponentNode: ResNode //this is a Base Component N
 
     private void ConstructImageNodes()
     {
+        _unkImageNode25 = new ImageNode
+        {
+            Size = new Vector2(40, 40),
+            Origin = new Vector2(20, 20),
+            AddColor = new Vector3(-0.251f, 0.251f, 0.502f),
+            PartId = 0,
+        };
+        PvPIconNodeTextureHelper.LoadPvPmksGuageTexture(_unkImageNode25);
         _unkImageNode22 = ConstructAndLoadArrowImageNode(new Vector2(230, 17), 0.098f);
         _unkImageNode21 = ConstructAndLoadArrowImageNode(new Vector2(200, 17), 0.098f);
         _unkImageNode20 = ConstructAndLoadArrowImageNode(new Vector2(170, 17), 0.146f);
@@ -230,20 +279,9 @@ public class PvPMKSMatchEventComponentNode: ResNode //this is a Base Component N
             Origin = new Vector2(20,20)
         };
 
-        _unkBaseComponentNode23 = new ResNode
-        {
-            //todo move to class
-            Size = new Vector2(40,40),
-            NodeFlags = NodeFlags.AnchorTop | NodeFlags.AnchorLeft | NodeFlags.Enabled | NodeFlags.EmitsEvents,
-        };
-        _unkBaseComponentNode13 = new ResNode
-        {
-            //todo move to class
-            Position = new Vector2(200,0),
-            Size = new Vector2(40,40),
-            NodeFlags = NodeFlags.AnchorTop | NodeFlags.AnchorLeft | NodeFlags.Enabled | NodeFlags.EmitsEvents,
-            Color = new Vector4(1,1,1,0)
-        };
+        _unkBaseComponentNode23 = new PVPMKSInfoMatchEventIconNode(new Vector2(0,0));
+        _unkBaseComponentNode13 = new PVPMKSInfoMatchEventIconNode(new Vector2(200,0));
+
 
         _unkResNode7 = new ResNode
         {
@@ -251,6 +289,215 @@ public class PvPMKSMatchEventComponentNode: ResNode //this is a Base Component N
             Color = new Vector4(1,1,1,0f),
             NodeFlags = NodeFlags.AnchorTop | NodeFlags.AnchorLeft | NodeFlags.Enabled | NodeFlags.EmitsEvents
         };
+    }
+
+    private void LoadTimelineForImageNodes()
+    {
+        _unkImageNode25.AddTimeline(new TimelineBuilder()
+                .BeginFrameSet(11, 20)
+                .AddEmptyFrame(11)
+                .EndFrameSet()
+                .Build());
+        _unkImageNode22.AddTimeline(new TimelineBuilder()
+                .BeginFrameSet(47, 55)
+                .AddFrame(47, alpha: 0)
+                .AddFrame(51, alpha: 51)
+                .AddFrame(55, alpha: 0)
+                .EndFrameSet()
+                .Build());
+        _unkImageNode21.AddTimeline(new TimelineBuilder()
+                .BeginFrameSet(44, 52)
+                .AddFrame(44, alpha: 0)
+                .AddFrame(48, alpha: 51)
+                .AddFrame(52, alpha: 0)
+                .EndFrameSet()
+                .Build());
+        _unkImageNode20.AddTimeline(new TimelineBuilder()
+                .BeginFrameSet(41, 49)
+                .AddFrame(41, alpha: 0)
+                .AddFrame(45, alpha: 51)
+                .AddFrame(49, alpha: 0)
+                .EndFrameSet()
+                .Build());
+        _unkImageNode19.AddTimeline(new TimelineBuilder()
+                .BeginFrameSet(38, 46)
+                .AddFrame(38, alpha: 0)
+                .AddFrame(41, alpha: 51)
+                .AddFrame(46, alpha: 0)
+                .EndFrameSet()
+                .Build());
+        _unkImageNode18.AddTimeline(new TimelineBuilder()
+                .BeginFrameSet(35, 43)
+                .AddFrame(35, alpha: 0)
+                .AddFrame(38, alpha: 51)
+                .AddFrame(43, alpha: 0)
+                .EndFrameSet()
+                .Build());
+        _unkImageNode17.AddTimeline(new TimelineBuilder()
+                .BeginFrameSet(32, 40)
+                .AddFrame(32, alpha: 0)
+                .AddFrame(35, alpha: 51)
+                .AddFrame(40, alpha: 0)
+                .EndFrameSet()
+                .Build());
+        _unkImageNode16.AddTimeline(new TimelineBuilder()
+                .BeginFrameSet(30, 38)
+                .AddFrame(30, alpha: 0)
+                .AddFrame(33, alpha: 51)
+                .AddFrame(38, alpha: 0)
+                .EndFrameSet()
+                .Build());
+        _unkImageNode15.AddTimeline(new TimelineBuilder()
+                .BeginFrameSet(27, 35)
+                .AddFrame(27, alpha: 0)
+                .AddFrame(30, alpha: 51)
+                .AddFrame(35, alpha: 0)
+                .EndFrameSet()
+                .Build());
+        _unkImageNode12.AddTimeline(new TimelineBuilder()
+                .BeginFrameSet(26, 55)
+                .AddFrame(26, alpha: 127)
+                .AddFrame(39, alpha: 0)
+                .AddFrame(55, alpha: 127)
+                .EndFrameSet()
+                .Build());
+        _unkImageNode11.AddTimeline(new TimelineBuilder()
+                .BeginFrameSet(26, 55)
+                .AddFrame(26, alpha: 127)
+                .AddFrame(39, alpha: 63)
+                .AddFrame(55, alpha: 127)
+                .EndFrameSet()
+                .Build());
+        _unkImageNode10.AddTimeline(new TimelineBuilder()
+                .BeginFrameSet(10, 25)
+                .AddFrame(10, scale: new Vector2(2, 2))
+                .AddFrame(19, scale: new Vector2(1, 1))
+                .AddFrame(10, alpha: 0)
+                .AddFrame(19, alpha: 127)
+                .EndFrameSet()
+                .Build());
+        _unkImageNode9.AddTimeline(new TimelineBuilder()
+                .BeginFrameSet(10, 25)
+                .AddFrame(10, scale: new Vector2(2, 2))
+                .AddFrame(19, scale: new Vector2(1, 1))
+                .AddFrame(10, alpha: 0)
+                .AddFrame(19, alpha: 127)
+                .EndFrameSet()
+                .Build());
+    }
+
+    private void LoadTimelineForNineGridNodes()
+    {
+        _unkNineGridNode27.AddTimeline(new TimelineBuilder()
+                .BeginFrameSet(10, 25)
+                .AddFrame(10, alpha: 0)
+                .AddFrame(14, alpha: 127)
+                .EndFrameSet()
+                .BeginFrameSet(26, 55)
+                .AddFrame(26, alpha: 127)
+                .EndFrameSet()
+                .BeginFrameSet(56, 70)
+                .AddFrame(56, alpha: 0)
+                .AddFrame(60, alpha: 127)
+                .EndFrameSet()
+                .BeginFrameSet(71, 120)
+                .AddFrame(71, alpha: 127)
+                .EndFrameSet()
+                .Build());
+        _unkNineGridNode14.AddTimeline(new TimelineBuilder()
+                .BeginFrameSet(26, 55)
+                .AddEmptyFrame(26)
+                .EndFrameSet()
+                .Build());
+    }
+
+    private void LoadTimelineForTextnodes()
+    {
+        _unkTextNode26.AddTimeline(new TimelineBuilder()
+                .BeginFrameSet(56, 70)
+                .AddFrame(56, position: new Vector2(0,0))
+                .AddFrame(61, position: new Vector2(48,0))
+                .AddFrame(56, alpha: 0)
+                .AddFrame(61, alpha: 231)
+                .EndFrameSet()
+                .BeginFrameSet(71, 120)
+                .AddFrame(71, position: new Vector2(48,0))
+                .AddFrame(98, position: new Vector2(48,0))
+                .AddFrame(120, position: new Vector2(48,0))
+                .AddFrame(71, alpha: 255)
+                .AddFrame(98, alpha: 127)
+                .AddFrame(120, alpha: 255)
+                .EndFrameSet()
+                .Build());
+        _unkTextNode8.AddTimeline(new TimelineBuilder()
+                .BeginFrameSet(1, 10)
+                .AddFrame(1, textOutlineColor: new Vector3(0, 153, 255))
+                .EndFrameSet()
+                .BeginFrameSet(11, 20)
+                .AddFrame(11, textOutlineColor: new Vector3(204, 55, 55))
+                .EndFrameSet()
+                .Build());
+        _unkTextNode6.AddTimeline(new TimelineBuilder()
+                .BeginFrameSet(10, 25)
+                .AddFrame(10, position: new Vector2(30,19))
+                .AddFrame(13, position: new Vector2(40,19))
+                .AddFrame(25, position: new Vector2(40,19))
+                .AddFrame(10, scale: new Vector2(2, 2))
+                .AddFrame(13, scale: new Vector2(1, 1))
+                .AddFrame(25, scale: new Vector2(1, 1))
+                .AddFrame(10, alpha: 0)
+                .AddFrame(13, alpha: 255)
+                .AddFrame(25, alpha: 255)
+                .AddFrame(10, textOutlineColor: new Vector3(230, 167, 58))
+                .AddFrame(13, textOutlineColor: new Vector3(230, 167, 58))
+                .AddFrame(25, textOutlineColor: new Vector3(157, 131, 91))
+                .EndFrameSet()
+                .BeginFrameSet(26, 55)
+                .AddFrame(26, position: new Vector2(40,19))
+                .AddFrame(39, position: new Vector2(40,19))
+                .AddFrame(55, position: new Vector2(40,19))
+                .AddFrame(26, scale: new Vector2(1, 1))
+                .AddFrame(39, scale: new Vector2(1, 1))
+                .AddFrame(55, scale: new Vector2(1, 1))
+                .AddFrame(26, alpha: 255)
+                .AddFrame(39, alpha: 191)
+                .AddFrame(55, alpha: 255)
+                .AddFrame(26, textOutlineColor: new Vector3(157, 131, 91))
+                .AddFrame(39, textOutlineColor: new Vector3(157, 131, 91))
+                .AddFrame(55, textOutlineColor: new Vector3(157, 131, 91))
+                .EndFrameSet()
+                .Build());
+        _unkTextNode5.AddTimeline(new TimelineBuilder()
+                .BeginFrameSet(13, 21)
+                .AddFrame(13, scale: new Vector2(1, 1))
+                .AddFrame(16, scale: new Vector2(1, 1.5f))
+                .AddFrame(21, scale: new Vector2(1, 2))
+                .AddFrame(13, alpha: 0)
+                .AddFrame(16, alpha: 127)
+                .AddFrame(21, alpha: 0)
+                .AddFrame(13, textOutlineColor: new Vector3(240, 142, 55))
+                .AddFrame(16, textOutlineColor: new Vector3(240, 142, 55))
+                .AddFrame(21, textOutlineColor: new Vector3(220, 0, 0))
+                .EndFrameSet()
+                .Build());
+        _unkTextNode4.AddTimeline(new TimelineBuilder()
+                .BeginFrameSet(10, 25)
+                .AddFrame(10, position: new Vector2(-50,20))
+                .AddFrame(15, position: new Vector2(100,20))
+                .AddFrame(25, position: new Vector2(110,20))
+                .AddFrame(10, alpha: 0)
+                .AddFrame(15, alpha: 255)
+                .AddFrame(25, alpha: 255)
+                .EndFrameSet()
+                .BeginFrameSet(26, 55)
+                .AddFrame(26, position: new Vector2(110,20))
+                .AddFrame(39, position: new Vector2(110,20))
+                .AddFrame(55, position: new Vector2(110,20))
+                .AddFrame(26, alpha: 255)
+                .AddFrame(39, alpha: 191)
+                .AddFrame(55, alpha: 255)
+                .EndFrameSet()
+                .Build());
     }
 
     private void LoadTimelineForResnodes()
@@ -300,6 +547,47 @@ public class PvPMKSMatchEventComponentNode: ResNode //this is a Base Component N
                 .AddFrame(36, position: new Vector2(0,0))
                 .AddFrame(32, alpha: 0)
                 .AddFrame(36, alpha: 255)
+                .EndFrameSet()
+                .Build());
+        _unkResNode24.AddTimeline(new TimelineBuilder()
+                .BeginFrameSet(1, 20)
+                .AddLabel(1, 17, AtkTimelineJumpBehavior.PlayOnce, 0)
+                .AddLabel(11, 101, AtkTimelineJumpBehavior.PlayOnce, 0)
+                .EndFrameSet()
+                .BeginFrameSet(15, 25)
+                .AddFrame(15, rotation: 6.2831855f)
+                .AddFrame(20, rotation: 3.1415927f)
+                .AddFrame(25, rotation: 0)
+                .AddFrame(15, scale: new Vector2(1, 1))
+                .AddFrame(20, scale: new Vector2(2.5f, 2.5f))
+                .AddFrame(25, scale: new Vector2(2, 2))
+                .AddFrame(15, alpha: 0)
+                .AddFrame(20, alpha: 255)
+                .AddFrame(25, alpha: 255)
+                .EndFrameSet()
+                .BeginFrameSet(26, 55)
+                .AddFrame(26, rotation: 0)
+                .AddFrame(55, rotation: 6.2831855f)
+                .AddFrame(26, scale: new Vector2(2, 2))
+                .AddFrame(55, scale: new Vector2(2, 2))
+                .AddFrame(26, alpha: 255)
+                .AddFrame(55, alpha: 255)
+                .EndFrameSet()
+                .Build());
+        _unkResNode7.AddTimeline(new TimelineBuilder()
+                .BeginFrameSet(1, 20)
+                .AddLabel(1, 101, AtkTimelineJumpBehavior.PlayOnce, 0)
+                .AddLabel(11, 102, AtkTimelineJumpBehavior.PlayOnce, 0)
+                .EndFrameSet()
+                .BeginFrameSet(10, 25)
+                .AddFrame(10, position: new Vector2(0,0))
+                .AddFrame(19, position: new Vector2(36,0))
+                .AddFrame(10, alpha: 0)
+                .AddFrame(19, alpha: 255)
+                .EndFrameSet()
+                .BeginFrameSet(26, 55)
+                .AddFrame(26, position: new Vector2(36,0))
+                .AddFrame(26, alpha: 229)
                 .EndFrameSet()
                 .Build());
     }
